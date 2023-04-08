b6d877230b170a20cb1156549115c86431fb950f
pablo-salituri <pablo_salituri@live.com.ar>
2023-04-08 20:45:26 -0300

    commit ej 3

--- arturito/src/containers/Main/index.tsx
+++ arturito/src/containers/Main/index.tsx
@@ -5,6 +5,7 @@ import SectionSelector from '../../components/SectionSelector';
 import Home from '../../components/HomeSection';
 import Planets from '../../components/PlanetsSection';
 import Starships from '../../components/Starships/Starships';
+import People from '../../components/People/People';
 
 const MainContainer = () => {
   const location = useLocation();
@@ -47,7 +48,8 @@ const MainContainer = () => {
         </Route>
 
         <Route path={paths.people.href}>
-          <div className="p-3">
+          <People />
+          {/* <div className="p-3">
             <p className="font-bold text-xl"># TODO</p>
             <p>
               Agregar tabla con los personajes sacados de la API. Mostrar para
@@ -59,7 +61,7 @@ const MainContainer = () => {
                 https://swapi.it/documentation#people
               </a>
             </p>
-          </div>
+          </div> */}
         </Route>
 
         <Route path={paths.home.href}>
